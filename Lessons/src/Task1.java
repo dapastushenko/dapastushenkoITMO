4792732c641b53c5011fff3d4b597649f0e95042
dapastushenko <dapastushenko@yandex.ru>
2017-10-12 14:37:13 +0300

    Task one finish

--- Lessons/src/Task1.java
+++ Lessons/src/Task1.java
@@ -1,2 +1,13 @@
+import java.util.Scanner;
+
 public class Task1 {
+    public static void main (String[] args){
+       // int q = 21;
+       // int w = 8;
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите последовательно два натуральных числа");
+        int q = in.nextInt();
+        int w = in.nextInt();
+        System.out.println(q+"/"+w+"="+q/w+" и "+q%w+" в остатке");
+    }
 }
