78a132f9066be03e99300e95e16b6414fc0063aa
dapastushenko <dapastushenko@yandex.ru>
2017-10-15 22:39:01 +0300

    finish

--- Lessons/src/Task2_12.java
+++ Lessons/src/Task2_12.java
@@ -1,17 +1,17 @@
 import java.util.Scanner;
 
 public class Task2_12 {
-    public static void main (String[] args) {
+    public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.println("Введите число:");
         int n = in.nextInt();
         int fact = 1;
         for (int i = 1; i <= n; i++) {
 
-            fact= fact*i;
+            fact = fact * i;
         }
-        if (fact!=0) {
+        if (fact != 0) {
             System.out.println(fact);
         }
-        }
+    }
 }
