044288999cba55a862fecdf3cbc2315ccc796e14
dapastushenko <dapastushenko@yandex.ru>
2017-10-15 13:32:38 +0300

    rename

--- Lessons/src/Task1_3.java
+++ Lessons/src/Task1_3.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Task3 {
+public class Task1_3 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.println("Введите дробное число, с дробной частью>0");
--- Lessons/src/Task1_4.java
+++ Lessons/src/Task1_4.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Task4 {
+public class Task1_4 {
     public static void main(String[] args){
         Scanner in=new Scanner(System.in);
         System.out.println("Введите целое 3-хзначное число:");
