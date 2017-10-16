e930f6eb086d0e85d767a005e9b20abaa48d06d5
dapastushenko <dapastushenko@yandex.ru>
2017-10-16 18:40:09 +0300

    Tasks lesson 2

--- Lessons/src/Task2_19.java
+++ Lessons/src/Task2_19.java
@@ -4,7 +4,7 @@ public class Task2_19 {
         int hours, minutes = 1;
 
         for (hours = 1; hours <= 23; hours++) {
-            for (minutes = 1; minutes <= 32; minutes++) {
+            for (minutes = 1; minutes <= 51; minutes++) {
                 if ((hours % 10 == minutes / 10) & (minutes % 10 == hours / 10)) {
                     count++;
                 }
