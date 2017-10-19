02cf5fbf286a712740b4a958e9441a13f02ea2b3
dapastushenko <dapastushenko@yandex.ru>
2017-10-19 19:31:22 +0300

    Tasks lesson 2

---
+++ Lessons/src/Lesson4/Adder.java
@@ -0,0 +1,14 @@
+package Lesson4;
+
+public class Adder {
+    int shag;
+    int first = 0;
+
+    public Adder(int q) {
+        shag = q;
+    }
+    public  int add() {
+     return   first += shag;
+
+    }
+}
