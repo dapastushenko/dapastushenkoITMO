e1bce74af31f9262ffb209baceff1b761d7001d3
dapastushenko <dapastushenko@yandex.ru>
2017-10-24 23:20:02 +0300

    finish

--- Lessons/src/Lesson6/List/List.java
+++ Lessons/src/Lesson6/List/List.java
@@ -3,8 +3,8 @@ package Lesson6.List;
 import com.sun.glass.ui.Size;
 
 public interface List {
-    void add();
-    void get();
-    void remove();
-    int Size=0;
+    void add(Object);
+    Object get(int);
+    Object remove(int);
+    void size(int);
 }
