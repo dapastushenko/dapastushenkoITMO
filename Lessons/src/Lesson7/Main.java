e721fb0119497a2887eaa2d5d3a3701eb6e3af86
dapastushenko <dapastushenko@yandex.ru>
2017-10-26 23:39:40 +0300

    finish first

--- Lessons/src/Lesson7/Main.java
+++ Lessons/src/Lesson7/Main.java
@@ -1,5 +1,7 @@
 package Lesson7;
 
+import java.lang.ref.SoftReference;
+
 public class Main {
     public static void main(String[] args) {
 // Создание экземпляра анонимного класса: new Operation {...}
@@ -9,8 +11,19 @@ public class Main {
                 return Math.abs(arg1) + Math.abs(arg2);
             }
         });
+        Accumulator umn =  new Accumulator(13, new Operation() {
+            @Override
+            public int apply(int arg1, int arg2) {
+                return arg1*arg2;
+            }
+        });
+
         acc.calculate(-10);
         acc.calculate(23);
         System.out.println(acc.getValue());
+
+        umn.calculate(2);
+        umn.calculate(3);
+        System.out.println(umn.getValue());
     }
 }
