41fc306c329ca197b3701439f1f4cca7837d6fd6
dapastushenko <dapastushenko@yandex.ru>
2017-10-29 21:36:00 +0300

    finish

--- Lessons/src/Lesson7/Accumulator.java
+++ Lessons/src/Lesson7/Accumulator.java
@@ -1,19 +1,19 @@
 package Lesson7;
 
-public class Accumulator {
+class Accumulator {
     private int value;
     private final Operation operation;
 
-    public Accumulator(int value, Operation operation) {
+    Accumulator(int value, Operation operation) {
         this.value = value;
         this.operation = operation;
     }
 
-    public void calculate(int arg) {
+    void calculate(int arg) {
         value = operation.apply(value, arg);
     }
 
-    public int getValue() {
+    int getValue() {
         return value;
     }
 
--- Lessons/src/Lesson7/Main.java
+++ Lessons/src/Lesson7/Main.java
@@ -1,22 +1,10 @@
 package Lesson7;
 
-import java.lang.ref.SoftReference;
-
 public class Main {
     public static void main(String[] args) {
 // Создание экземпляра анонимного класса: new Operation {...}
-        Accumulator acc = new Accumulator(100, new Operation() {
-            @Override
-            public int apply(int arg1, int arg2) {
-                return Math.abs(arg1) + Math.abs(arg2);
-            }
-        });
-        Accumulator umn =  new Accumulator(13, new Operation() {
-            @Override
-            public int apply(int arg1, int arg2) {
-                return arg1*arg2;
-            }
-        });
+        Accumulator acc = new Accumulator(100, (arg1, arg2) -> Math.abs(arg1) + Math.abs(arg2));
+        Accumulator umn =  new Accumulator(13, (arg1, arg2) -> arg1*arg2);
 
         acc.calculate(-10);
         acc.calculate(23);
